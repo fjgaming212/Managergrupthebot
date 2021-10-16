73f5282c1b06a12bc82b74682f0d0abfec5e9eef
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-10-16 08:59:47 +0700

    Create alive.py

---
+++ cinderella/modules/alive.py
@@ -0,0 +1,22 @@
+import re
+import os
+
+from telethon import events, Button
+from telethon import version as tlhver
+from pyrogram import version as pyrover
+from innexiaBot.events import register
+from innexiaBot import telethn as tbot
+
+PHOTO = "https://telegra.ph/file/0fa4ac715b5cf0c76dc27.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  ken = event.sender.first_name
+  SKYZU = "🍁 Holla I'm Gen Manager ! \n\n"
+  SKYZU += "🍁 I'm Working Properly \n\n"
+  SKYZU += "🍁 My Master : [Lord](https://t.me/FJ_GAMING) \n\n"
+  SKYZU += f"🍁 Telethon Version : {tlhver} \n\n"
+  SKYZU += f"🍁 Pyrogram Version : {pyrover} \n\n"
+  SKYZU += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Yagura_Managerbot?start=help"), Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Gen_Project_Support")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=SKYZU,  buttons=BUTTON)
